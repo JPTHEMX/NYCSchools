55b121dab7492fe0ea7338638adad1d2b1753913
Juan Pablo <juan.pablo@mavenir.com>
2022-08-09 17:50:16 -0500

    Initial Commit

---
+++ NYCSchools/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NYCSchools
+//
+//  Created by Juan Pablo Granados Garcia on 8/9/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
